5ca8b6013f52949e92f1747416e1411b40e81828
Eric Santos <eric.silvasantos44@gmail.com>
2022-09-12 20:53:21 -0300

    feat: requisito 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,10 @@
+db.resumoProdutos.insertOne({
+  franquia: "McDonalds",
+  totalProdutos: db.produtos.countDocuments({}),
+});
+
+db.resumoProdutos.find({}, {
+  _id: false,
+  franquia: true,
+  totalProdutos: true,
+});
