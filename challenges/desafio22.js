2cbff07c7f5b1a3242cf6166342cf931a2e01f5c
Eric Santos <eric.silvasantos44@gmail.com>
2022-09-12 21:40:14 -0300

    feat: requisito 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,21 @@
+db.produtos.updateMany({}, {
+  $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] },
+});
+
+db.produtos.updateOne({
+  nome: "Big Mac",
+}, {
+  $set: { "vendasPorDia.3": 60 },
+});
+
+db.produtos.updateMany({
+  tags: { $in: ["bovino"] },
+}, {
+  $set: { "vendasPorDia.6": 120 },
+});
+
+db.produtos.find({}, {
+  _id: false,
+  nome: true,
+  vendasPorDia: true,
+});
