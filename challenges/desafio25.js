8513e493f1c4d78768530ebb28c825ccb4e40c67
Eric Santos <eric.silvasantos44@gmail.com>
2022-09-12 22:11:44 -0300

    feat: requisito 26

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -2,15 +2,15 @@ db.produtos.updateMany({
   valoresNutricionais: { 
     $elemMatch: { 
       tipo: "sódio",
-      percentual: { $gte: 40},
+      percentual: { $gte: 40 },
     },
-  }
+  },
 }, {
-  $push: { tags: "muito sódio" } 
+  $push: { tags: "muito sódio" },
 });
 
 db.produtos.find({}, {
   _id: false,
   nome: true,
   tags: true,
-})
+});
